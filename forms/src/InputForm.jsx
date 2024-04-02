27d3fe9bd398edfc0f0ad8aaf8e15293d630eba4
Neil Comber <106420893+neilcomber@users.noreply.github.com>
2024-04-02 07:21:13 +0100

    more react hook form

--- forms/src/InputForm.jsx
+++ forms/src/InputForm.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react"
+import { useForm } from "react-hook-form"
 
 export default function InputForm({ addToList}) {
+
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+
+  console.log(watch("item")) // watch input value by passing the name of it
+ 
+
 
     const [formData, setFormData] = useState({
         item: "", 
         quantity: 0
     })
 
-    const [productIsValid, setProductIsValid] = useState(false);
+ 
 
   
     const handleInputData = (e)=> {
@@ -16,8 +30,8 @@ export default function InputForm({ addToList}) {
         })
     }
 
-    const handleSubmit = (e)=>{
-            e.preventDefault();
+    const haveSubmit = (e)=>{
+            // e.preventDefault();
             addToList(formData)
             setFormData({
                 item: "", 
@@ -25,13 +39,15 @@ export default function InputForm({ addToList}) {
             })
     }
 
+    console.log(errors)
     
     return (
         
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(haveSubmit)}>
     <label htmlFor="item">product name</label>
-    <input type="text" name="item" id="item" placeholder="item" value={formData.item} onChange={handleInputData}/>
-    {!productIsValid && <p style={{color: "red"}}>Product name cannot be empty</p>}
+    <input type="text" name="item" {...register("item", { required: "name is required"})} id="item" placeholder="item" value={formData.item} onChange={handleInputData}/>
+    {errors.item && <span>product name is required</span>}
+
     <label htmlFor="quantity">quantity</label>
     <input type="number" name="quantity" id="quantity" placeholder="quantity" value={formData.quantity} onChange={handleInputData}/>
     <button>Add To List</button>
