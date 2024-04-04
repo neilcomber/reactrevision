41e0ee3ee7c7e730585afd03abaa140d4b9cc959
Neil Comber <106420893+neilcomber@users.noreply.github.com>
2024-04-04 07:12:32 +0100

    finished github search

--- effects/src/GithubSearch.jsx
+++ effects/src/GithubSearch.jsx
@@ -1,13 +1,41 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 import GithubSearchForm from "./GithubSearchForm";
 
+const BASE_URL = "https://api.github.com/users"
+
 export default function GithubSearch() {
 
+const [username, setUsername] = useState("")
+const [profile, setProfile] = useState({ data: null, isLoading: true})
 
+useEffect(function fetchUserOnNameChange(){
+
+    async function fetchUser(){
+        const userResult = await axios.get(`${BASE_URL}/${username}`);
+        setProfile({data: userResult.data, isLoading: false})
+    }
+    fetchUser();
+
+}, [username])
+
+const search = (name) => {
+    setProfile({data: null, isLoading: true})
+    setUsername(name);
+}
+
+if (profile.isLoading) return (
+    <div>
+        <i>Loading....</i>
+        <GithubSearchForm search={search}/>
+    </div>
+)
 
     return (
         <div>
-             <GithubSearchForm />
+            <h2>{profile.isLoading ? "loading..." : profile.data.login}</h2>
+            <img src={!profile.isLoading && profile.data.avatar_url}/>
+             <GithubSearchForm search={search}/>
         </div>
     )
 }
--- effects/src/GithubSearchForm.jsx
+++ effects/src/GithubSearchForm.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 
-export default function GithubSearchForm() {
+export default function GithubSearchForm({search}) {
 
     const [name, setName] = useState("")
+
+    const handleChange= (e)=> {
+        setName(e.target.value)
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        search(name);
+        setName("")
+        
+    }
 
     return (
         <div>
     <h3>Search Form</h3>
-    <form action="" onSubmit={()=>{handleSubmit}}>
+    <form action="" onSubmit={handleSubmit}>
         <label htmlFor="name">name</label>
-        <input type="text" name="name" id="name" value={name} onChange={(e)=>{setName(e.target.value)}}/>
+        <input type="text" name="name" id="name" value={name} onChange={handleChange}/>
+        <button>Search</button>
     </form>
     </div>
     )
